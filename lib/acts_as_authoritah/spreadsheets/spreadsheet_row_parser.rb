c98a25d2db916e3edbd2e2cc42c6c7ecf6eb71f0
Unnikrishnan KP <unni.tallman@gmail.com>
2012-04-03 12:29:41 +0530

    spreadsheet row parser added

--- lib/acts_as_authoritah/spreadsheets/spreadsheet_row_parser.rb
+++ lib/acts_as_authoritah/spreadsheets/spreadsheet_row_parser.rb
@@ -1,3 +1,23 @@
 class ActsAsAuthoritah::SpreadsheetRowParser
+  def initialize(row)
+    @row = row
+  end
   
+  def scope
+    @row[0]
+  end
+  
+  def controller
+    @row[1]
+  end
+  
+  def action
+    @row[2]
+  end
+  
+  def access_rights
+    @row[4..-1].collect{|x|
+      x == 'x' || x == "X" || x == "*"
+    }
+  end
 end
--- spec/acts_as_authoritah/spreadsheets/spreadsheet_row_parser_spec.rb
+++ spec/acts_as_authoritah/spreadsheets/spreadsheet_row_parser_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 
 describe ActsAsAuthoritah::SpreadsheetRowParser do
-  it "should take a valid row and return the scope" do
-    
+  before :each do 
+    @valid_row = ["scope1::scope2", "Dummy", "edit", "test", "x", "X", nil]  
   end
+  
+  it "should take a valid row and return the scope" do
+    ActsAsAuthoritah::SpreadsheetRowParser.new(@valid_row).scope.should eq "scope1::scope2"
+  end
+  
+  it "should take a valid row and return the controller" do
+    ActsAsAuthoritah::SpreadsheetRowParser.new(@valid_row).controller.should eq "Dummy"
+  end
+  
+  it "should take a valid row and return the action" do
+    ActsAsAuthoritah::SpreadsheetRowParser.new(@valid_row).action.should eq "edit"
+  end
+  
+  it "should take a valid row and return the access rights" do
+    ActsAsAuthoritah::SpreadsheetRowParser.new(@valid_row).access_rights.should eq [true, true, false]
+  end
+
 end
