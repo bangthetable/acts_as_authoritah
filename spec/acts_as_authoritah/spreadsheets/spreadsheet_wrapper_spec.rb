2ba7aadeae29efd7ca31e672dcc807df391f5a04
Unnikrishnan KP <unni.tallman@gmail.com>
2012-04-03 13:11:50 +0530

    one more spec added

--- spec/acts_as_authoritah/spreadsheets/spreadsheet_wrapper_spec.rb
+++ spec/acts_as_authoritah/spreadsheets/spreadsheet_wrapper_spec.rb
@@ -4,4 +4,12 @@ describe ActsAsAuthoritah::SpreadsheetWrapper do
   it "should be able to open a spreadsheet and convert to an array of AccessRules" do
     ActsAsAuthoritah::SpreadsheetWrapper.new("spec/data/default.xls").to_access_rules.first.class.should eq ActsAsAuthoritah::AccessRule
   end
+  
+  it "ssadf" do
+    access_rules = ActsAsAuthoritah::SpreadsheetWrapper.new("spec/data/default.xls").to_access_rules
+    ActsAsAuthoritah::AccessControlList.new(access_rules).store.should eq({
+      "scope1::scope2::DummyController#edit"=>{"admin"=>true, "anonymous"=>false, "super_admin"=>true}, 
+      "scope3::scope4::AnotherController#update"=>{"admin"=>false, "anonymous"=>true, "super_admin"=>true}
+      })
+  end
 end
